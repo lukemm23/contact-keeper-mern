d1aa2279900e9d1189e04951f5112c4264b9f126
lukemm23 <lukemm23@gmail.com>
2020-01-01 01:05:39 -0600

    added items rendering hard coded items.

--- src/components/contacts/ContactItem.js
+++ src/components/contacts/ContactItem.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 const ContactItem = ({ contact }) => {
-    const { id, name, email, phone, type } = contact;
+    const { name, email, phone, type } = contact;
 
     return (
         <div className='card bg-light'>
@@ -16,19 +16,25 @@ const ContactItem = ({ contact }) => {
             <ul className='list'>
                 {email && (
                     <li>
-                        <i className='fas fa-envelope-open'></i> {email}
+                        <i className='fas fa-envelope-open'></i>  {email}
                     </li>
                 )}
                 {phone && (
                     <li>
-                        <i className='fas fa-phone'></i> {phone}
+                        <i className='fas fa-phone'></i>  {phone}
                     </li>
                 )}
             </ul>
+            <p>
+                <button className='btn btn-dark btn-sm'>Edit</button>
+                <button className='btn btn-danger btn-sm'>Delete</button>
+            </p>
         </div>
     )
 }
 
-
+ContactItem.propTypes =  {
+    contact: PropTypes.object.isRequired
+}
 
 export default ContactItem;
