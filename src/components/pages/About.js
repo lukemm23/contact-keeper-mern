72f473088d8dd3cf84f1d08708026cf22af57d5b
lukemm23 <lukemm23@gmail.com>
2019-12-31 22:30:35 -0600

    new repo project setup

---
+++ src/components/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const About = () => {
+    return(
+        <div>
+            <p className="my-1">
+                This is a full stack react App for keeping contacts
+            </p>
+            <p className="bg-dark p">
+                <strong>Version:</strong>1.0.0
+            </p>
+        </div>
+    )
+}
+
+
+
+export default About;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Home = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+export default Home;
