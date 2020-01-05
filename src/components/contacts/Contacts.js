926abf454fc251c0dc4e887b40600d3601a323fb
lukemm23 <lukemm23@gmail.com>
2020-01-05 17:06:58 -0600

    add/get/clear contacts complete

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,25 +1,32 @@
-import React, { Fragment,useContext } from 'react';
+import React, { Fragment,useContext, useEffect } from 'react';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import ContactContext from '../context/contact/contactContext';
 import ContactItem from './ContactItem';
+import Spinner from '../layouts/Spinner';
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
 
-    const {contacts, filtered} = contactContext;
+    const {getContacts, loading, contacts, filtered} = contactContext;
 
-    if(contacts.length ===0){
+    useEffect(()=>{
+        getContacts();
+        //eslint-disable-next-line
+    }, []);
+
+    if(contacts !== null && contacts.length ===0 && !loading){
         return <h4>Please add a contact</h4>
     }
 
     return(
         <Fragment>
-            <TransitionGroup>
-            {filtered !== null ? filtered.map(contact => (<CSSTransition timeout={500} classNames="item" key={contact.id}><ContactItem
+            {contacts !== null && !loading ? (<TransitionGroup>
+            {filtered !== null ? filtered.map(contact => (<CSSTransition timeout={500} classNames="item" key={contact._id}><ContactItem
              contact={contact} /></CSSTransition>)) : contacts.map(contact => (
-                <CSSTransition timeout={500} classNames="item" key={contact.id}><ContactItem contact={contact} /></CSSTransition>
+                <CSSTransition timeout={500} classNames="item" key={contact._id}><ContactItem contact={contact} /></CSSTransition>
             ) )}
-            </TransitionGroup>
+            </TransitionGroup>) : <Spinner/>}
+            
         </Fragment>
     )
 }
--- src/components/context/contact/ContactState.js
+++ src/components/context/contact/ContactState.js
@@ -1,8 +1,10 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import axios from 'axios';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
+    GET_CONTACTS,
+    CLEAR_CONTACTS,
     ADD_CONTACT,
     DELETE_CONTACT,
     SET_CURRENT,
@@ -10,48 +12,63 @@ import {
     UPDATE_CONTACT,
     FILTER_CONTACTS,
     CLEAR_FILTER,
+    CONTACT_ERROR
 } from '../types';
 
 const ContactState = props => {
     const initialState = {
-        contacts: [
-            {
-                id: 1,
-                name: 'jill johnson',
-                email: 'jill@gmail.com',
-                phone: '111-111-1111',
-                type: 'personal'
-            },
-            {
-                id: 2,
-                name: 'sara watson',
-                email: 'sara@gmail.com',
-                phone: '222-222-2222',
-                type: 'personal'
-            },
-            {
-                id: 3,
-                name: 'harry white',
-                email: 'harry@gmail.com',
-                phone: '333-333-3333',
-                type: 'professional'
-            },
-        ],
+        contacts: null,
         current: null,
-        filtered: null
+        filtered: null,
+        error: null
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
 
+    //get contacts
+    const getContacts = async () => {
+        try {
+            const res = await axios.get('/api/contacts');
+
+            dispatch({type:GET_CONTACTS, payload: res.data});
+        } catch (err) {
+            dispatch({
+                type: CONTACT_ERROR,
+                payload: err.response.msg
+            });
+        }
+        
+    };
+
     //add contact
-    const addContact = contact => {
-        contact.id= uuid.v4();
-        dispatch({type:ADD_CONTACT, payload: contact});
+    const addContact = async contact => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/contacts', contact, config);
+
+            dispatch({type:ADD_CONTACT, payload: res.data});
+        } catch (err) {
+            dispatch({
+                type: CONTACT_ERROR,
+                payload: err.response.msg
+            });
+        }
+        
     };
 
     //delete contact
     const deleteContact = id => {
         dispatch({type:DELETE_CONTACT, payload: id});
+    };
+
+    //clear contacts
+    const clearContacts = () => {
+        dispatch({type: CLEAR_CONTACTS});
     };
 
     //set current contact
@@ -83,13 +100,16 @@ const ContactState = props => {
                 contacts: state.contacts, 
                 current: state.current,
                 filtered: state.filtered,
+                error: state.error,
+                getContacts,
                 addContact,
                 deleteContact,
                 setCurrent,
                 clearCurrent,
                 updateContact,
                 filterContacts,
-                clearFilter
+                clearFilter,
+                clearContacts
             }}>
             {props.children}
         </ContactContext.Provider>
