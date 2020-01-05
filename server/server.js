82b09c3cae70bad50d11ccac16e6a6d138f5b704
lukemm23 <lukemm23@gmail.com>
2020-01-05 17:37:45 -0600

    prepped for deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require ('path');
 
 //importing mongoose and DB connection
 const mongoose = require('mongoose');
@@ -15,6 +16,13 @@ app.get('/', (req, res) => res.json({msg:'welcome to the ContactKeeper API...'})
 app.use('/api/users', require('../server/routes/users'));
 app.use('/api/contacts', require('../server/routes/contacts'));
 app.use('/api/auth', require('../server/routes/auth'));
+
+//serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res)=> res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5500;
 
